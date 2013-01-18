053b5f3a52af33b8a24b963a23d902b45331a2a9
Karma <boris.isolet@gmail.com>
2013-01-18 15:18:46 +0800

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Snapguide::Application.config.secret_token = 'd5d1701a969a1d4b08983d975d3ac4153d112da7e2131f2996368ef3d640c732047243611d1140094dbbc8a6df31476e258f1e4c879152f5b5ecb58bcc0c58bc'
